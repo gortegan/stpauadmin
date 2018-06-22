6f394376854caf2a114b3031a612d36741f1e21c
Gerard Ortega <gortega@BCN.softcatalunya.com>
2018-06-22 16:36:40 +0200

    First one

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" id="logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#logo{
+  margin-top: 100px;
+  width: 150px;
+}
+.bottom-nav .btn{
+  width: 50%;
+  max-width: 50%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card color="primary">
+    <v-bottom-nav :value="true" :active.sync="e1" absolute color="transparent">
+      <v-btn flat color="teal" value="recent">
+        <span>Recent</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+      <v-btn flat color="teal" value="favorites">
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Administració de Sant Pau del Camp'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+  .bottom-nav.bottom-nav--absolute.bottom-nav--active.transparent {
+    position: fixed;
+  }
+  .phone-viewport {
+    width: 322px;
+    height: 200px;
+    display: inline-flex;
+    align-items: flex-end;
+    overflow: hidden;
+    border: 1px solid rgba(#000, .26);
+    background: rgba(#000, .06);
+  }
+</style>
